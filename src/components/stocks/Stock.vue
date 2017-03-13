c1a9cbba386a0e9489c61013dfb25e9eab19c232
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-03-13 14:28:54 -0400

    make stock components dynamic

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -3,16 +3,21 @@
 		<div class="panel panel-success">
 			<div class="panel-heading">
 				<h3 class="panel-title">
-					NAME
-					<small>(Price: PRICE)</small>
+					{{stock.name}}
+					<small>(Price: {{stock.price}})</small>
 				</h3>
 			</div>
 			<div class="panel-body">
 				<div class="pull-left">
-					<input type="number" class="form-control" placeholder="quantity">
+					<input type="number" class="form-control" placeholder="quantity" v-model="quantity">
 				</div>
 				<div class="pull-right">
-					<button class="btn btn-success">Buy</button>
+					<button 
+							class="btn btn-success"
+							@click="buyStock"
+							:disabled="quantity <= 0">
+							Buy
+					</button>
 				</div>
 			</div>
 		</div>
@@ -21,6 +26,22 @@
 
 <script type="text/javascript">
 	export default {
-		
+		props: ['stock'],
+		data() {
+			return {
+				quantity: 0
+			}
+		},
+		methods: {
+			buyStock() {
+				const order = {
+					stockId: this.stock.id,
+					stockPrice: this.stock.price,
+					quantity: this.quantity
+				};
+				console.log(order);
+				this.quantity = 0;
+			}
+		}
 	}
 </script>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="row">
-		<app-stock v-for="stock in stocks"></app-stock>
+		<app-stock v-for="stock in stocks" :stock="stock"></app-stock>
 	</div>
 </template>
 
