181f30fa9519e12b74af9611b5d5094c13fbb488
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-03-13 16:02:53 -0400

    install axios and make it and the backend url accessible globally

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import axios from 'axios';
+window.axios = axios;
+window.baseUrl = 'https://vue-stock-trader-eef39.firebaseio.com/';
 
 import App from './App.vue'
 import {routes} from './routes.js';
 import store from './store/store.js';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 Vue.filter('currency', value => {
 	return '$' + value.toLocaleString();
