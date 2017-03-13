3858287840f92dd42667d97a1e1603989eeee29c
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-03-13 14:02:32 -0400

    create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
+  <div>  
+    <app-header></app-header>		
     <div class="container">
     	<router-view></router-view>
     </div>
+  </div>
 </template>
 
 <script>
-
+		import Header from './components/Header.vue';
     export default {
-
+    	components: {
+    		appHeader: Header
+    	}
     }
 </script>
 
